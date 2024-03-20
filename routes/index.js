4205d1232b6eea50729039b8938a0152b5f1388f
TUNGNT <nguyenthanhtunnghutechsg@gmail.com>
2024-03-20 15:32:40 +0700

    solve conflict

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-router.get('/a', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+router.use('/books',require('./books'));
+router.use('/users',require('./users'));
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,5 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-router.get('/b', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
 module.exports = router;
